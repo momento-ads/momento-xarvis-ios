32b36b92908f92996b05809ff69881966fc4cc61
조민현 <cho.minhyun@cashwalk.io>
2023-10-19 16:52:49 +0900

    Add Dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,12 +12,10 @@ let package = Package(
             targets: ["MomentoXarvis-iOS"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/momento-ads/momento-ios-sdk.git", from: "1.0.5"),
+        .package(url: "https://github.com/thezooom/Xarvis_iOS.git", from: "0.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MomentoXarvis-iOS",
             dependencies: []),
